f5c8ea126f68cdca1071a64755b22e1a03b4260d
eteen <eteensmash@gmail.com>
2024-02-11 16:22:07 -0800

    added logic for when the user clicks

--- main/script.js
+++ main/script.js
@@ -19,13 +19,21 @@ document.addEventListener("DOMContentLoaded",function(){
         for(let i = 0; i < board.length; i++){
             let cell = document.createElement('div');
             cell.classList.add('cell');
-            board[i]=i;
+            cell.setAttribute('data-index', i);
             gameBoard.appendChild(cell);
         }
 
        
 
     };
+    function player1(event){
+        let clickedCellIndex = event.target.getAttribute('data-index');
+        if(board[clickedCellIndex] === '' && gameStarted){
+            board[clickedCellIndex]='X';
+            event.target.textContent = 'X';
+        }
+    }
+    document.addEventListener('click', player1);
 
     let startBtn = document.querySelector('.start-btn');
     startBtn.addEventListener("click",function(){
